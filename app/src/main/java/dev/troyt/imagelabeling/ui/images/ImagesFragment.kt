1ce886cd890bd7d4845131a2482cd28a9f77c1c3
Trien Tran <aunhi55@gmail.com>
2021-12-08 00:38:17 +1030

    images screen done

--- app/src/main/java/dev/troyt/imagelabeling/ui/images/ImagesFragment.kt
+++ app/src/main/java/dev/troyt/imagelabeling/ui/images/ImagesFragment.kt
@@ -1,6 +1,5 @@
 package dev.troyt.imagelabeling.ui.images
 
-import android.content.ClipData
 import android.content.Intent
 import android.os.Bundle
 import android.provider.MediaStore
@@ -12,11 +11,12 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import dev.troyt.imagelabeling.databinding.FragmentImagesBinding
-import dev.troyt.imagelabeling.ui.Recognition
 import dev.troyt.imagelabeling.ui.defaultDispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
@@ -28,12 +28,9 @@ class ImagesFragment : Fragment() {
     // Contains the recognition result. Since  it is a viewModel, it will survive screen rotations
     private val viewModel: ImagesViewModel by viewModels()
     private var _binding: FragmentImagesBinding? = null
-    private var imageCount: Int = 0
     private lateinit var viewAdapter: ImagesAdapter
-    private lateinit var clipData: ClipData
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -42,75 +39,35 @@ class ImagesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentImagesBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val rootView: View = binding.root
 
         binding.pickPhotoBtn.setOnClickListener { onPickPhoto() }
+        binding.clearBtn.setOnClickListener { onClearBtnClick() }
 
-        // Initialising the resultRecyclerView and its linked viewAdaptor
+        // Initialising the RecyclerView and its linked Adapter
         viewAdapter = ImagesAdapter(requireContext())
         binding.recyclerView.adapter = viewAdapter
+
         // initialize an instance of linear layout manager
         val layoutOrientation =
             (binding.recyclerView.layoutManager as LinearLayoutManager).orientation
         val dividerItemDecoration = DividerItemDecoration(requireContext(), layoutOrientation)
         binding.recyclerView.addItemDecoration(dividerItemDecoration)
-
+        Log.d(tag, "trien")
         lifecycleScope.launch {
-            viewModel.uiState.collect {
-                //recogViewModel.addData(it)
-                val adapter = binding.recyclerView.adapter as ImagesAdapter
-                val lastRowIndex = adapter.itemCount
-                adapter.submitList(it)
-                // adapter.notifyItemInserted(lastRowIndex)
-                Log.d("trien1", it.toString())
-
+            // repeatOnLifecycle launches the block in a new coroutine every time the
+            // lifecycle is in the STARTED state (or above) and cancels it when it's STOPPED.
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                // Trigger the flow and start listening for values.
+                // Note that this happens when lifecycle is STARTED and stops
+                // collecting when the lifecycle is STOPPED
+                viewModel.recognitionList.collect {
+                    viewAdapter.submitList(it)
+                }
             }
         }
-        /* lifecycleScope.launch {
-             // repeatOnLifecycle launches the block in a new coroutine every time the
-             // lifecycle is in the STARTED state (or above) and cancels it when it's STOPPED.
-             repeatOnLifecycle(Lifecycle.State.STARTED) {
-                 // Trigger the flow and start listening for values.
-                 // Note that this happens when lifecycle is STARTED and stops
-                 // collecting when the lifecycle is STOPPED
-                 viewModel.uiState.collect {
-                         //recogViewModel.addData(it)
-                         Log.d("trien1", it.toString())
-                         val adapter = binding.recyclerView.adapter as ImagesAdapter
-                         val lastRowIndex = adapter.itemCount
-                         adapter.submitList(it)
-                         adapter.notifyItemInserted(lastRowIndex)
-                     }
-             }
-
-             *//*lifecycleScope.launch(Dispatchers.Default) {
-                predictImageFlow(clipData).
-                recogViewModel.updateData(recognitionList)
-                Log.d("trien", recognitionList.toString())
-                }
-*//*
-            *//* for (i in 0 until clipData.itemCount) {
-                 val imageUri: Uri = clipData.getItemAt(i).uri
-                 val bitmap = imageUri.toScaledBitmap(requireContext(), 224, 224)
-                 if (bitmap != null) {
-                     //predictImage(imageUri, bitmap)
-
-                 }
-             }*//*
-        }*/
-        // Attach an observer on the LiveData field of recognitionList
-        // This will notify the recycler view to update every time when a new list is set on the
-        // LiveData field of recognitionList.
-//        recogViewModel.recognitionList.observe(viewLifecycleOwner, {
-//            viewAdapter.submitList(null)
-//            viewAdapter.submitList(it)
-//            Log.d("trien1", it.toString())
-//        }
-        //       )
-        return root
+        return rootView
     }
-
-    // Trigger gallery selection for a photo
 
     private fun onPickPhoto() {
         // Create intent for picking a photo from the gallery
@@ -119,29 +76,22 @@ class ImagesFragment : Fragment() {
         resultLauncher.launch(intent)
     }
 
+    private fun onClearBtnClick() {
+        viewModel.clearAllData()
+    }
+
     private var resultLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == AppCompatActivity.RESULT_OK) {
                 result.data?.clipData?.let { clipData ->
-                    imageCount = clipData.itemCount
-                    Log.d("trien2", imageCount.toString())
-
                     viewModel.inferImages(requireContext(), clipData)
+                        .catch { Log.e(tag, it.localizedMessage ?: "some error") }
+                        .onEach { viewModel.addData(it) }
                         .flowOn(defaultDispatcher)
-                        .onEach {
-                            //recogViewModel.addData(it)
-                            val recognitionList = mutableListOf<Recognition>()
-                            recognitionList.add(it)
-                            viewModel.updateFlowData(recognitionList)
-                            Log.d("trien3", recognitionList.toString())
-                        }.onCompletion { Log.d("trien4", "fragment canceled") }
                         .launchIn(lifecycleScope)
-                    // Start a coroutine in the lifecycle scope
-
                 }
             }
         }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/dev/troyt/imagelabeling/ui/images/ImagesViewModel.kt
+++ app/src/main/java/dev/troyt/imagelabeling/ui/images/ImagesViewModel.kt
@@ -4,8 +4,6 @@ import android.content.ClipData
 import android.content.Context
 import android.net.Uri
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.label.ImageLabeling
@@ -14,48 +12,31 @@ import dev.troyt.imagelabeling.R
 import dev.troyt.imagelabeling.ui.Recognition
 import dev.troyt.imagelabeling.ui.home.HomeViewModel
 import dev.troyt.imagelabeling.ui.toScaledBitmap
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.*
 
 class ImagesViewModel : ViewModel() {
 
     private val tag = HomeViewModel::class.simpleName
-    // This is a LiveData field. Choosing this structure because the whole list tend to be updated
-    // at once in ML and not individual elements. Updating this once for the entire list makes
-    // sense.
 
     // Backing property to avoid state updates from other classes
-    private val _uiState = MutableStateFlow<MutableList<Recognition>>(mutableListOf())
+    private val _recognitionList = MutableStateFlow<MutableList<Recognition>>(mutableListOf())
 
     // The UI collects from this StateFlow to get its state updates
-    val uiState: StateFlow<MutableList<Recognition>> get() = _uiState
-
-    private val _recognitionList = MutableLiveData<MutableList<Recognition>>(mutableListOf())
-    val recognitionList: LiveData<MutableList<Recognition>> get() = _recognitionList
-
-    fun updateFlowData(recognitions: MutableList<Recognition>) {
-        _uiState.value = recognitions
-        Log.d("trienz", uiState.value.toString())
-    }
-
-    fun updateData(recognitions: MutableList<Recognition>) {
-        _recognitionList.value = recognitions
-    }
-
-    fun addFlowData(recognition: Recognition) {
-        _recognitionList.value?.add(recognition)
-    }
+    val recognitionList: StateFlow<MutableList<Recognition>> get() = this._recognitionList
 
     fun addData(recognition: Recognition) {
-        _recognitionList.value?.add(recognition)
+        val currentList = _recognitionList.value.toMutableList()
+        currentList.add(recognition)
+        _recognitionList.value = currentList
     }
 
     fun clearAllData() {
-        _recognitionList.value?.clear()
+        _recognitionList.value = mutableListOf()
     }
 
+    @ExperimentalCoroutinesApi
     fun inferImages(
         context: Context,
         clipData: ClipData,
@@ -96,6 +77,6 @@ class ImagesViewModel : ViewModel() {
                     Log.e(tag, it.localizedMessage ?: "some error")
                 }
         }
-        this.awaitClose()
+        awaitClose { labeler.close() }
     }
 }
